9f3cb1530a5b7176ea2413b003e3971989a745ef
huss <coralloc8@dingtalkcom>
2024-06-05 16:41:15 +0800

    新增redis读取

--- coral-test/spring-native-test/src/main/java/com/coral/test/spring/natives/service/impl/EventLogServiceImpl.java
+++ coral-test/spring-native-test/src/main/java/com/coral/test/spring/natives/service/impl/EventLogServiceImpl.java
@@ -1,6 +1,7 @@
 package com.coral.test.spring.natives.service.impl;
 
 import com.coral.test.spring.natives.convert.EventLogConvert;
+import com.coral.test.spring.natives.core.redis.ReactiveRedisTemplateFactory;
 import com.coral.test.spring.natives.core.utils.id.IdCreatorFactory;
 import com.coral.test.spring.natives.dto.PageInfo;
 import com.coral.test.spring.natives.dto.req.EventLogPageQueryDTO;
@@ -8,17 +9,24 @@ import com.coral.test.spring.natives.dto.req.EventLogQueryDTO;
 import com.coral.test.spring.natives.dto.req.EventLogSaveDTO;
 import com.coral.test.spring.natives.dto.res.EventLogFullInfoDTO;
 import com.coral.test.spring.natives.dto.res.EventLogInfoDTO;
+import com.coral.test.spring.natives.enums.EventStatus;
 import com.coral.test.spring.natives.model.EventLog;
 import com.coral.test.spring.natives.repository.EventLogRepository;
 import com.coral.test.spring.natives.service.EventLogService;
-import jakarta.annotation.Resource;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -30,12 +38,17 @@ import java.util.stream.Collectors;
  * @className EventLogServiceImpl
  */
 @Slf4j
+@RequiredArgsConstructor
 @Service
 public class EventLogServiceImpl implements EventLogService {
 
-    @Resource
-    private EventLogRepository eventLogRepository;
+    private final EventLogRepository eventLogRepository;
 
+    @Qualifier("reactiveRedisTemplate")
+    private final ReactiveRedisTemplate reactiveRedisTemplate;
+
+
+    @Cacheable(value = "EventLog")
     @Override
     public Mono<EventLogInfoDTO> findEventLog(String eventLogId) {
         return eventLogRepository.findEventLogByEventLogId(eventLogId)
@@ -51,8 +64,19 @@ public class EventLogServiceImpl implements EventLogService {
 
     @Override
     public Flux<EventLogFullInfoDTO> findEventFullLogs(EventLogQueryDTO eventLogQuery) {
-        return eventLogRepository.findEventFullLogs(eventLogQuery)
-                .flatMap(log -> Mono.justOrEmpty(EventLogConvert.INSTANCE.convertEventLogFullInfoDTO(log)));
+        boolean eventEnd = Objects.nonNull(eventLogQuery.getStatus()) && !EventStatus.PROCESSING.equals(eventLogQuery.getStatus());
+        boolean accurateEventEnd = StringUtils.isNotBlank(eventLogQuery.getEventLogId()) && eventEnd;
+        if (!accurateEventEnd) {
+            return eventLogRepository.findEventFullLogs(eventLogQuery)
+                    .flatMap(log -> Mono.justOrEmpty(EventLogConvert.INSTANCE.convertEventLogFullInfoDTO(log)));
+        }
+        final String cacheKey = String.join("_", "EventFullLog::", eventLogQuery.getEventLogId(), eventLogQuery.getStatus().getCode().toString());
+        log.info("##### 触发缓存存储动作，key：{}", cacheKey);
+
+        return ReactiveRedisTemplateFactory.getInstance(reactiveRedisTemplate).getIfAbsent(cacheKey,
+                eventLogRepository.findEventFullLogs(eventLogQuery).map(EventLogConvert.INSTANCE::convertEventLogFullInfoDTO),
+                Duration.ofMinutes(1)
+        );
     }
 
     @Override
